56c5b28adca26455c5b3fb0b939723c75ff8412a
Tim Walsh <timothyryanwalsh@gmail.com>
2018-04-18 17:23:54 -0400

    Re-add increment_progress_bar variable added by pyqt4topyqt5

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ class CheckableDirModel(QDirModel):
         return QDirModel.setData(self, index, value, role)
 
 class SIPThread(QThread):
+    increment_progress_bar = pyqtSignal('QString')
 
     def __init__(self, dirs_to_process, destination, bagfiles, piiscan, output_dir):
         QThread.__init__(self)
